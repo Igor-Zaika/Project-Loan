045ede294ec4e6cc40e49f70f13a3f2cb5827cf4
Igor <leonidlee777@gmail.com>
2021-12-02 18:07:25 +0200

    add video player

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider';
+import playVideo from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const player = new playVideo('.showup .play', '.overlay');
+    player.init();
 });
